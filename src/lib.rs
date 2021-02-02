2c4b96dcbbf63e2c38f153f12d801a06cd64562b
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-02-02 20:26:30 +0300

    comment about vlq

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 use std::time::Duration;
 use std::net::SocketAddr;
 
+// TODO
+/*
+Recommended VLQ impls:
+1. https://docs.rs/vint64/1.0.1/vint64/
+2. https://docs.rs/vlq-rust/0.4.0/vlq_rust/
+3. https://github.com/ergoplatform/sigma-rust/blob/master/sigma-ser/src/vlq_encode.rs
+
+Also search for VLQ + ZigZag impl. It's necessary, because type are being encoded in
+Handshake message using VLQ + ZigZag.
+*/
+
 struct Handshake {
     agent_name: String,
     version: Version,
@@ -10,7 +21,7 @@ struct Handshake {
     features: Option<Vec<PeerFeature>>
 }
 
-struct Version;
+struct Version(u32, u32, u32);
 
 enum PeerFeature {}
 
