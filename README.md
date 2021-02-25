556d38ab5c62d128c74b8878b3e3e2557ab36bfa
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-02-25 09:11:02 +0300

    test expected values from scala node HS msgs stated

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TODO
+## Main
 1. handshaking fn
 2. resolve minor todos
+## Postponed
+1. Proper tests for HS with timestamp   
+2. Optimize VLQ (de)encoder
--- src/hs.rs
+++ src/hs.rs
@@ -14,6 +14,7 @@ pub struct Handshake {
 
 // todo-minor HandshakeParser/SerializerError
 impl Handshake {
+    // todo-crucial max size?
     pub fn parse(data: &[u8]) -> Result<Self, VlqEncodingError> {
         let mut hs_reader = HSSpecReader::new(default_vlq_reader(data));
 
@@ -86,10 +87,15 @@ mod tests {
     #[test]
     fn vectors() {
         let bytes = [
+            // Handshake(PeerSpec(ergoref,Version(4,0,5),ergo-mainnet-4.0.0,None,Vector()),1613639654979) and Vector(ModeFeature(utxo,true,None,-1), SessionIdPeerFeature([(1, 0, 2, 4),-7226886467503878579))
             hex_to_bytes("c3bcaca3fb2e076572676f726566040005126572676f2d6d61696e6e65742d342e302e300002100400010001030e01000204e5c6abfafabc87cbc801"), // my
+            // Handshake(PeerSpec(ergoref,Version(3,3,6),mainnet-seed-node-sf,Some(/165.227.26.175:9030),Vector()),1613639655059) + Vector(ModeFeature(utxo,true,None,-1), SessionIdPeerFeature([(1, 0, 2, 4),-2393537216959524988))
             hex_to_bytes("93bdaca3fb2e076572676f726566030306146d61696e6e65742d736565642d6e6f64652d73660108a5e31aafc64602100400010001030d01000204f7c1e5d8dadac6b742"), // remote=/165.227.26.175:9030
+            // Handshake(PeerSpec(ergoref,Version(4,0,5),ergo-mainnet-4.0.1,Some(/213.239.193.208:9030),Vector()),1613639659870) + (ModeFeature(utxo,true,None,-1), SessionIdPeerFeature((1, 0, 2, 4),6155961833357488951))
             hex_to_bytes("dee2aca3fb2e076572676f726566040005126572676f2d6d61696e6e65742d342e302e310108d5efc1d0c64602100400010001030e01000204eecc9582ffaaafeeaa01"), // 213.239.193.208:9030
+            // Handshake(PeerSpec(ergoref,Version(3,3,6),mainnet-seed-node-toronto,Some(/159.89.116.15:9030),Vector()),1613639665005) + (ModeFeature(utxo,true,None,-1), SessionIdPeerFeature(тот же ,-8301048747963648041))
             hex_to_bytes("ed8aada3fb2e076572676f726566030306196d61696e6e65742d736565642d6e6f64652d746f726f6e746f01089f59740fc64602100400010001030e01000204d1a098d9dff69fb3e601"), // 159.89.116.15:9030
+            // Handshake(PeerSpec(ergoref,Version(3,3,6),ergo-mainnet-4.0.0,None,Vector()),1613639669987) + Vector(ModeFeature(utxo,true,None,-1), SessionIdPeerFeature(то же самое,3120095637531038426))
             hex_to_bytes("e3b1ada3fb2e076572676f726566030306126572676f2d6d61696e6e65742d342e302e300002100400010001030d01000204b4cbc4c4f19ce7cc56") //83.212.114.255:9030
         ];
         for b in bytes.iter() {
@@ -101,6 +107,7 @@ mod tests {
     #[test]
     fn test_hs_ergo_vector() {
         let hs_bytes = hex_to_bytes("bcd2919cee2e076572676f726566030306126572676f2d6d61696e6e65742d332e332e36000210040001000102067f000001ae46");
+        // from scala ModeFeature(utxo,true,None,-1), LocalAddressPeerFeature(/127.0.0.1:9006))
         let a = Handshake::parse(&hs_bytes).ok().expect("ergo reference test vector failed");
         println!("{:?}", a);
         println!("{:?}", hs_bytes);
