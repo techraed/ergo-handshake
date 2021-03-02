ef70c2ae8e4821affd4ab6abf7fda799116662cb
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-03-02 10:52:24 +0300

    ip6 tested

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub use features::{FeatureParseError, FeatureSerializeError, Features, FeaturesError, Mode, PeerFeature, SessionId};
 pub use messages::{Handshake, HsSpecReaderError, HsSpecWriterError};
 pub use models::{MagicBytes, PeerAddr, ShortString, Version};
+pub use hs::{handshaking, HandshakingError};
 
 mod encoding;
 mod features;
